43866832f41beec12cc5fe562392ad0c40f221ba
Ayotayo1689 <badejopatrick7@gmail.com>
2025-02-11 17:17:42 +0100

    hello

--- future-save-app/src/components/SideBar.jsx
+++ future-save-app/src/components/SideBar.jsx
@@ -15,11 +15,13 @@ const style = {
   top: "50%",
   left: "50%",
   transform: "translate(-50%, -50%)",
-  width: 400,
+  width: "90vw",
+  maxWidth:400,
   bgcolor: "background.paper",
   border: "none",
   outline: "none",
   p: 4,
+  borderRadius:2
 };
 
 const SideBar = ({ toggleSidebar }) => {
@@ -128,12 +130,19 @@ const SideBar = ({ toggleSidebar }) => {
         aria-describedby="modal-modal-description"
       >
         <Box sx={style}>
-          <Typography id="modal-modal-title" variant="h6" component="h2">
-            Text in a modal
-          </Typography>
-          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
-          </Typography>
+          <div className="w-14 h-14 rounded-full m-auto bg-[#f7e2e2] border  flex justify-center items-center">
+            <LogoutIcon color="red" />
+          </div>
+          <h2 className="m-auto font-semibold text-2xl text-center mt-4">
+            Logout
+          </h2>
+          <p className="m-auto text-center mt-2">
+            Are you sure you want to logout?
+          </p>
+          <div className="flex flex-col gap-6 mt-6">
+          <button className="bg-[red] p-2 rounded-lg text-white">Yes, Continue</button>
+          <button className="border border-[red] p-2 rounded-lg text-[red]">Cancel</button>
+          </div>
         </Box>
       </Modal>
     </>
